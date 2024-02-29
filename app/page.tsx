76bfbb4e5a5e30d4a641f340cbc046288c0b8696
Ahmed <heydude08080@gmail.com>
2024-02-29 18:56:51 +0500

    added about.tsx and platform.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-
+import Platform from "./components/Platform";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Platform />
     </>
   );
 }
