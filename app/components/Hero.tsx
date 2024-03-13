9fb9521d2abe71ddc1686d978c9819a13378f501
Ahmed <heydude08080@gmail.com>
2024-03-13 16:39:14 +0500

    added bg gradient

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,13 @@
 "use client";
+import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
+
 import Card from "@/app/components/Card";
 import { title } from "process";
 import { TypewriterEffectSmoothDemo } from "@/app/components/Heading";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/fade-animation"
+import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/fade-animation";
 import Link from "next/link";
-
 
 export default function Hero() {
   const cardProps1 = {
@@ -32,84 +33,98 @@ export default function Hero() {
     triggerOnce: true,
   });
 
-
- 
-
   return (
-
     <>
-    <section id="/">
-      <div className="min-h-screen min-w-screen text-white overflow-x-hidden">
-        {/* text */}
-        <div>
-          {/* First heading */}
-          <motion.div
-      initial="hidden"
-      animate="show"
-      viewport={{ once: true }}
-      variants={{
-        hidden: {},
-        show: {
-          transition: {
-            staggerChildren: 0.15,
-          },
-        },
-      }} className="flex flex-col  font-bold md:gap-5 gap-0 md:min-w-[949px] text-center justify-center md:mt-32 mt-28 ">
-            {/* <h1 className="text-[2rem] md:text-[4rem]">The Home of</h1>
+      <section id="/">
+      <BackgroundGradientAnimation className="h-[10rem]">
+        <div className="min-h-screen min-w-screen text-white overflow-x-hidden z-10">
+          {/* text */}
+          <div>
+            {/* First heading */}
+            <motion.div
+              initial="hidden"
+              animate="show"
+              viewport={{ once: true }}
+              variants={{
+                hidden: {},
+                show: {
+                  transition: {
+                    staggerChildren: 0.15,
+                  },
+                },
+              }}
+              className="flex flex-col  font-bold md:gap-5 gap-0  text-center justify-center md:mt-32 mt-28 "
+            >
+              {/* <h1 className="text-[2rem] md:text-[4rem]">The Home of</h1>
             <h2 className="text-[2.8rem] md:text-[4rem] text-[#f89b1f] ">
               {" "}
               Benefit Mine
             </h2> */}
-            <motion.h1 variants={FADE_DOWN_ANIMATION_VARIANTS} className="text-secondary text-[2.5rem] mr-[1rem] ml-[1rem] md:text-[4rem]">The Home of <span className="text-[2.9rem] md:text-[4rem] text-[#f89b1f]">Benefit Mine</span></motion.h1>
-            <div>
-            <motion.p variants={FADE_DOWN_ANIMATION_VARIANTS} className="text-[#f89b1f]  text-center text-sm md:text-2xl mt-4 md:mt-2">Your Business
-          <span className="text-secondary"> Our Responsibility</span></motion.p>
-          </div>
-          <div>
-          <Link href="https://presale.benefitmine.io/" target="_blank">
-          <motion.button variants={FADE_DOWN_ANIMATION_VARIANTS}  className=" relative inline-flex mt-6 md:mt-4 h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                          
-            <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#ffcc00_0%,#EBECF0_50%,#f89b1f_100%)] " />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-[1.5rem] py-[1rem] md:text-[1rem] text-[0.8rem]  text-white backdrop-blur-3xl font-semibold">
-              BUY $BFM
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="ml-[0.25rem] w-5 h-4"
+              <motion.h1
+                variants={FADE_DOWN_ANIMATION_VARIANTS}
+                className="text-secondary text-[2.5rem] mr-[1rem] ml-[1rem] md:text-[4rem]"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                />
-              </svg>
-            </span>
-          </motion.button>
-            </Link>
-          </div>
-          </motion.div>
-          {/* Second Heading */}
-          <div className="text-[0.9rem] mt-[-1rem]  flex font-bold text-center justify-center">
-            {/* <h1 className="text-[#f89b1f]">Your Business</h1>
+                The Home of{" "}
+                <span className="text-[2.9rem] md:text-[4rem] text-[#f89b1f]">
+                  Benefit Mine
+                </span>
+              </motion.h1>
+              <div>
+                <motion.p
+                  variants={FADE_DOWN_ANIMATION_VARIANTS}
+                  className="text-[#f89b1f]  text-center text-sm md:text-2xl mt-4 md:mt-2"
+                >
+                  Your Business
+                  <span className="text-secondary"> Our Responsibility</span>
+                </motion.p>
+              </div>
+              <div>
+                <Link href="https://presale.benefitmine.io/" target="_blank">
+                  <motion.button
+                    variants={FADE_DOWN_ANIMATION_VARIANTS}
+                    className=" relative inline-flex mt-6 md:mt-4 h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                  >
+                    <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#ffcc00_0%,#EBECF0_50%,#f89b1f_100%)] " />
+                    <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-[1.5rem] py-[1rem] md:text-[1rem] text-[0.8rem]  text-white backdrop-blur-3xl font-semibold">
+                      BUY $BFM
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="ml-[0.25rem] w-5 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                        />
+                      </svg>
+                    </span>
+                  </motion.button>
+                </Link>
+              </div>
+            </motion.div>
+            {/* Second Heading */}
+            <div className="text-[0.9rem] mt-[-1rem]  flex font-bold text-center justify-center">
+              {/* <h1 className="text-[#f89b1f]">Your Business</h1>
           <h2 className=" ">Our Responsibility</h2> */}
-            {/* <TypewriterEffectSmoothDemo /> */}
+              {/* <TypewriterEffectSmoothDemo /> */}
+            </div>
           </div>
-        </div>
 
-        {/* Cards */}
-        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-0 place-items-center mt-20 "> */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[7em]   ">
-          <Card one={cardProps1} />
-          <Card one={cardProps2} />
-          <div className="md:col-span-2 flex justify-center xl:col-span-1">
-    <Card one={cardProps3} />
-  </div>
-
-        </div>
-      </div></section>
+          {/* Cards */}
+          {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-0 place-items-center mt-20 "> */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[7em]   ">
+            <Card one={cardProps1} />
+            <Card one={cardProps2} />
+            <div className="md:col-span-2 flex justify-center xl:col-span-1">
+              <Card one={cardProps3} />
+            </div>
+          </div>
+        </div></BackgroundGradientAnimation>
+      </section>
     </>
   );
 }
