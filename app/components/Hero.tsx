a026b15dd7909e4285807a657eb4b054f291f8bd
muhammadammar24 <muhammadammar2492@gmail.com>
2024-03-14 15:12:02 +0500

    Fixed Buttons

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -105,7 +105,7 @@ export default function Hero() {
                 </Link> */}
 
 <motion.a
-                    variants={FADE_DOWN_ANIMATION_VARIANTS}  href="#Signup" className="mt-4 relative inline-flex items-center justify-start px-8 py-3 overflow-hidden font-bold rounded-full group">
+                    variants={FADE_DOWN_ANIMATION_VARIANTS}  href="#Signup" className="mt-6 md:mt-4 relative inline-flex items-center justify-start px-8 py-3 overflow-hidden font-bold rounded-full group">
 <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
 <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-primary opacity-100 group-hover:-translate-x-8"></span>
 <span className="relative w-full text-left text-secondary transition-colors duration-200 ease-in-out group-hover:text-gray-900">Sign Up</span>
